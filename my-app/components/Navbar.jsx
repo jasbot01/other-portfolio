9e70c0884ff90768cb178ce135cd0a4025b30460
Jasmine-Snyder <93411708+Jasmine-Snyder@users.noreply.github.com>
2026-06-04 13:58:29 -0700

    light gradient surrounding navigation bar

--- my-app/components/Navbar.jsx
+++ my-app/components/Navbar.jsx
@@ -5,6 +5,10 @@ import React from 'react'
 const Navbar = () => {
     return (
         <>
+        <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
+            <Image src={assets.header_bg_color} alt='' className='w-full'/>
+        </div>
+        
         {/* this is to keep the NAV bar horizontal at the top of the page */}
         <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'> 
             <a href="#top">
@@ -12,14 +16,15 @@ const Navbar = () => {
                 cursor-pointer mr-14'/>
             </a>
 
-            <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3'>
+            {/* add light shadow behind nav bar */}
+            <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50'>
                 <li><a className="nav-link" href="#top">Home</a></li>    
                 <li><a className="nav-link" href="#about">About me</a></li>
                 <li><a className="nav-link" href="#work">My Work</a></li>
                 <li><a className="nav-link" href="#contact">Contact me</a></li>    
             </ul>
             <div>
-                <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo'>Contact <Image src={assets.arrow_icon} alt=""className='w-3'/></a>
+                <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 nav-link font-Ovo'>Contact <Image src={assets.arrow_icon} alt=""className='w-3'/></a>
             </div> 
         </nav>
         </>
