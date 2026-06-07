4159565978300de45624f3d35a104a91f2006d91
Jasmine-Snyder <93411708+Jasmine-Snyder@users.noreply.github.com>
2026-06-06 21:51:24 -0700

    added those alt descriptions yoooo

--- my-app/components/Header.jsx
+++ my-app/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
     return (
         <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4'>
             <div>
-                <Image src={assets.jas_profile} alt='' className='rounded-full w-32'/>
+                <Image src={assets.jas_profile} alt='jas' className='rounded-full w-32'/>
             </div>
             
             <h3 className={`${ovo.className} flex items-end gap-2 text-xl md:text-2xl mb-3`}>
@@ -30,11 +30,11 @@ const Header = () => {
             <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
                 <a href="#contact"
                 className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center'
-                >contact me<Image src={assets.right_arrow_white} alt=''
+                >contact me<Image src={assets.right_arrow_white} alt='contact button'
                 className='w-4'/></a>
 
                 <a href="/sample-resume.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2'> 
-                my resume<Image src={assets.download_icon} alt=''
+                my resume<Image src={assets.download_icon} alt='resume download button'
                 className='w-4'/></a> 
             
             </div> 
