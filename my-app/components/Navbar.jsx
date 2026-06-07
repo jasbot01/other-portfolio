c9da8bb956a5459b1280fedd3dab66f8b67fa50c
Jasmine-Snyder <93411708+Jasmine-Snyder@users.noreply.github.com>
2026-06-06 21:59:24 -0700

    stop it with the alt msgs

--- my-app/components/Navbar.jsx
+++ my-app/components/Navbar.jsx
@@ -6,13 +6,13 @@ const Navbar = () => {
     return (
         <>
         <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
-            <Image src={assets.header_bg_color} alt='' className='w-full'/>
+            <Image src={assets.header_bg_color} alt='header' className='w-full'/>
         </div>
         
         {/* this is to keep the NAV bar horizontal at the top of the page */}
         <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'> 
             <a href="#top">
-                <Image src={assets.logo} className='w-28 alt=""
+                <Image src={assets.logo} className='w-28 alt="cursor"
                 cursor-pointer mr-14'/>
             </a>
 
