6c05825b408cf872138d5859b19d8a4adb29e1e1
Jasmine-Snyder <93411708+Jasmine-Snyder@users.noreply.github.com>
2026-06-09 17:53:59 -0700

    recent update contact me part

--- my-app/components/Contact.jsx
+++ my-app/components/Contact.jsx
@@ -12,13 +12,13 @@ const Contact = () => {
     return (
         <div id='contact' className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/
         footer-bg-color.png")] bg-no-repeat bg-center bg-[length:90%_auto]'>
-            <h4 className={`${ovo.className} text-center mb-2 text-lg`}>
+            <h1 className={`${ovo.className} text-center mb-2`}>
                 Contact me 
-            </h4>
+            </h1>
             
-            <h2 className={`${ovo.className} text-center text-5xl`}>
+            <h4 className={`${ovo.className} text-center text-5xl`}>
                 Get in touch 
-            </h2>
+            </h4>
 
             <p className={`${ovo.className} text-center max-w-2xl mx-auto mt-5 mb-12`}>
                 I'd love to hear from you! 
