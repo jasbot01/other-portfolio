69b6bc00ab8d6274d495bfb07ff556a6e6c73b23
Jasmine-Snyder <93411708+Jasmine-Snyder@users.noreply.github.com>
2026-06-09 19:22:04 -0700

    links now working to github

--- my-app/components/Work.jsx
+++ my-app/components/Work.jsx
@@ -34,16 +34,22 @@ const Work = () => {
                         <div className='bg-white w-10/12 rounded-md absolute bottom-5
                         left-1/2 -translate-x-1/2 py-3 px-5 flex items-center
                         justify-between duration-500 group-hover:bottom-7'>
+
+                            <a href={project.git_url} target="_blank">   
                             <div>
                                 <h2 className='font-semibold'>{project.title}</h2> 
                                 <p className='text-sm text-gray-700'>{project.description}</p> 
-                                <a src={project.git_url}></a> 
-                            </div> 
+                                </div> 
+
+                                
+                            </a> 
+
                             <div className='border rounded-full border-black w-9
-                            aspect-square flex items-center justify-center shadow-[2px_2px_0_#000]
-                            group-hover:bg-lime-300 transition'>
+                                    aspect-square flex items-center justify-center shadow-[2px_2px_0_#000]
+                                    group-hover:bg-lime-300 transition'>
                                 <Image src={assets.send_icon} alt='send icon' className='w-5'/>
                             </div>
+
                         </div>  
                     </div>
                 ))}
